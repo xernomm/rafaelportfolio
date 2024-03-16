59b399d8911b331b16bbf26164f333683750395c
xernomm <rafaelrichie03@gmail.com>
2024-03-16 20:01:55 +0700

    Search feature

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -5,14 +5,36 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import xernom from '../img/xernomm.png'
 import * as I from 'react-bootstrap-icons'
 import '../styles/Images.css'
+import { Button, FloatingLabel, Form } from 'react-bootstrap';
+import { useRef } from 'react';
 
 export const Header = () => {
+
+    const inputRef = useRef(null);
 
     const scrollToSection = (id) => {
         const element = document.getElementById(id);
         if (element) {
             element.scrollIntoView({ behavior: 'smooth' });
         }
+    };
+
+    const scrollToElement = (element) => {
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        const searchText = inputRef.current.value.trim().toLowerCase();
+        const elements = document.querySelectorAll("*");
+        elements.forEach(element => {
+            if (element.textContent.toLowerCase().includes(searchText)) {
+                scrollToElement(element);
+            }
+        });
+        inputRef.current.value = ''; // Clear input field after search
     };
 
     return(
@@ -37,7 +59,16 @@ export const Header = () => {
                             </Nav>
                         </Navbar.Collapse>
                         <Navbar.Collapse className="justify-content-end">
-                                                        
+                                                        <Form onSubmit={handleSearch}>
+                                <div className="d-flex justify-content-center align-items-center searchForm">
+                                    <FloatingLabel controlId="floatingInput" label="Search.." className="text-muted ">
+                                        <Form.Control ref={inputRef} type="text" placeholder="Search" style={{ width: "135%", position: "relative", zIndex: "2" }} />
+                                    </FloatingLabel>
+                                    <Button type='submit' className='get-started-btn searchBtn' style={{ position: "relative", zIndex: "4" }}>
+                                        <I.Search className='lead'/>
+                                    </Button>
+                                </div>
+                            </Form>
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
